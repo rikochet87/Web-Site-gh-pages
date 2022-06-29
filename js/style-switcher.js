6f2fff02e8bab7c72e480f74ff1ab44314b12ae4
matias <matias3282@hotmail.com>
2022-06-29 16:48:44 -0300

    Updates

---
+++ js/style-switcher.js
@@ -0,0 +1,39 @@
+(function () {
+  let styleSwitcherToggler = document.querySelector(".style-switcher-toggler");
+  styleSwitcherToggler.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+  });
+  window.addEventListener("scroll", () => {
+    if (document.querySelector(".style-switcher").classList.contains("open")) {
+      document.querySelector(".style-switcher").classList.remove("open");
+    }
+  });
+  
+})();
+
+function setActiveStyle(color){
+    let alternateStyles = document.querySelectorAll(".alternate-style");
+    alternateStyles.forEach((style) => {
+      if (color === style.getAttribute("title")) {
+        style.removeAttribute("disabled");
+      } else {
+        style.setAttribute("disabled", "true");
+      }
+    });
+}
+
+(function(){
+    let dayNight = document.querySelector(".day-night");
+    dayNight.addEventListener("click", ()=>{
+        dayNight.querySelector("i").classList.toggle("fa-sun");
+        dayNight.querySelector("i").classList.toggle("fa-moon");
+        document.body.classList.toggle("dark")
+    })
+    window.addEventListener("load", ()=>{
+        if(document.body.classList.contains("dark")){
+            dayNight.querySelector("i").classList.add("fa-sun");
+        }else{
+            dayNight.querySelector("i").classList.add("fa-moon");
+        }
+    })
+})();
